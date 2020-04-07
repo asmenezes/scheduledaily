3a7fcb046194103c974e8b7af91922efd21d2ee1
asmenezes <asmenezes23@gmail.com>
2020-04-07 14:45:07 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Main from "./components/main.js";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/" render={props => <Main />} />
+    </Router>
+  );
+}
+
+export default App;
