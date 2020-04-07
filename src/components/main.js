19ffd4eb81708145ccb5a05a6801a8e1ba38776d
asmenezes <asmenezes23@gmail.com>
2020-04-07 15:04:06 -0400

    Added Components and organization

--- src/components/main.js
+++ src/components/main.js
@@ -3,7 +3,10 @@ import React, {useState} from "react";
 function Main() {
   return (
     <React.Fragment>
+      <div class="backHeader"> </div>
       <div id="creationSidebar" class="sidebar"></div>
+      <div id="schedulePreview"></div>
+      <div id="taskCreator"></div>
     </React.Fragment>
   );
 }
