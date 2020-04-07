6cba09430db46a0ded0c47d766f0f66c65b8e311
asmenezes <asmenezes23@gmail.com>
2020-04-07 15:29:26 -0400

    Sized Sidebar

--- src/components/creation/creationsidebar.js
+++ src/components/creation/creationsidebar.js
@@ -1,7 +1,11 @@
 import React, {useState} from "react";
 
 function CreationSidebar() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <div class="sidebar" id="creationsidebar"></div>
+    </React.Fragment>
+  );
 }
 
 export default CreationSidebar;
