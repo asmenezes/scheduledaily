0e03813eca6c9a78211e5ff938f99c359a84b22c
asmenezes <asmenezes23@gmail.com>
2020-04-08 13:52:02 -0400

    more build

---
+++ build/precache-manifest.01bf43a60ea588b1bb303011a6a63756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82de56a8e6e9ea3774354bc20831e29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "255e4901cef47ff46fa3",
+    "url": "./static/css/main.7609b3a6.chunk.css"
+  },
+  {
+    "revision": "7843a6903b690cc9af36",
+    "url": "./static/js/2.adf97272.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.adf97272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "255e4901cef47ff46fa3",
+    "url": "./static/js/main.6c03c8a7.chunk.js"
+  },
+  {
+    "revision": "74c5ccb13945e8f60bc0",
+    "url": "./static/js/runtime-main.ac167c34.js"
+  }
+]);
