7375666dbda140a0302bc4775ab228b40d1ebd0e
asmenezes <asmenezes23@gmail.com>
2020-04-08 13:49:05 -0400

    Updated To Proper Home Page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route exact path="/" render={props => <Main />} />
+      <Route exact path="" render={props => <Main />} />
     </Router>
   );
 }
