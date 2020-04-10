de455044a7f3a6c9e9f5d7e7759357c075a8ec84
asmenezes <asmenezes23@gmail.com>
2020-04-10 11:04:27 -0400

    Created Task Block

--- src/components/creation/taskcreator.js
+++ src/components/creation/taskcreator.js
@@ -3,7 +3,14 @@ import React, {useState} from "react";
 function TaskCreator() {
   return (
     <React.Fragment>
-      <div id="taskcreator"></div>
+      <div id="taskcreator">
+        <input
+          class="tasknameinput taskcreationinput"
+          placeholder="Task Name"
+          type="text"
+        />
+        <div class="sub class input inserter component here"></div>
+      </div>
     </React.Fragment>
   );
 }
---
+++ src/components/taskblock.js
@@ -0,0 +1,7 @@
+import React, {useState} from "react";
+
+function TaskBlock() {
+  return <React.Fragment></React.Fragment>;
+}
+
+export default TaskBlock;
