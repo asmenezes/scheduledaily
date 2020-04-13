25d9aa7a125e070a0e60eb55b5bae4b2618c9f04
asmenezes <asmenezes23@gmail.com>
2020-04-13 11:46:46 -0400

    Formatting Task Creator

--- src/components/creation/taskcreator.js
+++ src/components/creation/taskcreator.js
@@ -1,31 +1,46 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function TaskCreator() {
   return (
     <React.Fragment>
       <div id="taskcreator">
-        <input
-          className="tasknameinput taskcreationinput"
-          placeholder="Task Name"
-          type="text"
-        />
-        <label for="starttime"></label>
-        <input
-          className="taskcreationinput timeinput"
-          type="time"
-          name="starttime"
-        />
-        <label for="endtime"></label>
-        <input
-          className="taskcreationinput timeinput"
-          type="time"
-          name="endtime"
-        />
+        <div class="lineup">
+          <div>
+            <input
+              className="tasknameinput taskcreationinput"
+              placeholder="Task Name"
+              type="text"
+            />
+          </div>
+          <div>
+            <label for="starttime"></label>
+            <input
+              className="taskcreationinput timeinput"
+              type="time"
+              name="starttime"
+            />
+            <label for="endtime"></label>
+            <input
+              className="taskcreationinput timeinput"
+              type="time"
+              name="endtime"
+            />
+          </div>
+          <div>
+            <div className="create list of subtasks"></div>
+            <div className="creater of subtasks">
+              <input
+                className="subtasknameinput taskcreationinput"
+                placeholder="Subtask Name"
+                type="text"
+              />
+            </div>
+          </div>
+        </div>
         <textarea cols="28" rows="6" class="tasknotesinput taskcreationinput">
           Notes:
         </textarea>
-        <div className="create list of subtasks"></div>
-        <div className="creater of subtasks"></div>
+        <div></div>
       </div>
     </React.Fragment>
   );
