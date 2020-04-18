d7c17bc849bf85c45246fd4430315af3b57aa7d4
asmenezes <asmenezes23@gmail.com>
2020-04-18 11:27:05 -0400

    Format Top of Current Task Display

--- src/components/schedule/currenttaskdisplay.js
+++ src/components/schedule/currenttaskdisplay.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-
-function CurrentDisplay (){
-  return(
-    <div id = "currentdisplay">
+function CurrentDisplay() {
+  return (
+    <div id="currentdisplay">
+      <div id="currenttasktop">
+        <h1 id="currentdisplayname"> CurrentTask </h1>
+        <button id="nexttaskbutt">Next Task</button>
+      </div>
+      <div id="currentdiplaysub"> </div>
+      <div id="currentdisplaynotes"> </div>
     </div>
-  )
+  );
 }
 
-export default CurrentDisplay
+export default CurrentDisplay;
