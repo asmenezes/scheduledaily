f003c56d05d9fe81ecd765a215b4fa69c4741e2a
asmenezes <asmenezes23@gmail.com>
2020-04-19 14:54:54 -0400

    New Build

---
+++ build/precache-manifest.7a20fd877945693b9ce0b31ad271bebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd2439c75e1a63ba2b1dc4a740e5d1f",
+    "url": "/scheduledaily/build/index.html"
+  },
+  {
+    "revision": "399f9a1a1b9c2a848fe8",
+    "url": "/scheduledaily/build/static/css/main.7962373a.chunk.css"
+  },
+  {
+    "revision": "7843a6903b690cc9af36",
+    "url": "/scheduledaily/build/static/js/2.adf97272.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scheduledaily/build/static/js/2.adf97272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399f9a1a1b9c2a848fe8",
+    "url": "/scheduledaily/build/static/js/main.ed422691.chunk.js"
+  },
+  {
+    "revision": "f91eede47369cc12f63f",
+    "url": "/scheduledaily/build/static/js/runtime-main.834d5ef1.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.14275bdb72e8857179554d7ff05abb73.js"
+  "/scheduledaily/build/precache-manifest.7a20fd877945693b9ce0b31ad271bebf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scheduledaily/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
