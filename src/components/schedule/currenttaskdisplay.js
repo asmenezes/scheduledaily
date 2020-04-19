2bd92dc1bb4cd92448caa3bad8376e12f026e7ff
asmenezes <asmenezes23@gmail.com>
2020-04-19 11:11:03 -0400

    Formatting Current task Display

--- src/components/schedule/currenttaskdisplay.js
+++ src/components/schedule/currenttaskdisplay.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SubtaskDisplay from "./subtaskdisplay";
 
 function CurrentDisplay() {
   return (
@@ -7,8 +8,15 @@ function CurrentDisplay() {
         <h1 id="currentdisplayname"> CurrentTask </h1>
         <button id="nexttaskbutt">Next Task</button>
       </div>
-      <div id="currentdiplaysub"> </div>
-      <div id="currentdisplaynotes"> </div>
+      <div id="currentdiplaysub">
+        <SubtaskDisplay />
+        <SubtaskDisplay />
+        <SubtaskDisplay />
+        <SubtaskDisplay />
+      </div>
+      <div id="currentdisplaynotes">
+        <textarea className="dayNotes fillup">Task Notes:</textarea>{" "}
+      </div>
     </div>
   );
 }
---
+++ src/components/schedule/subtaskdisplay.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function SubtaskDisplay() {
+  return (
+    <div class="subtaskdisplay">
+      <input type="checkbox" class="check" />
+      <p>Subtask Name SUbtask Name</p>
+      <p>indic</p>
+    </div>
+  );
+}
+
+export default SubtaskDisplay;
