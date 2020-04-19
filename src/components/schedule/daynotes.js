f91deb4b75924c1a3ad78e8eec1b4088a79766ec
asmenezes <asmenezes23@gmail.com>
2020-04-19 12:12:07 -0400

    Font Sizing

--- src/components/schedule/daynotes.js
+++ src/components/schedule/daynotes.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function DayNotes() {
-  return <textarea id="daynotes">Task Notes:</textarea>;
+  return <textarea id="daynotes">Day Notes:</textarea>;
 }
 
 export default DayNotes;
