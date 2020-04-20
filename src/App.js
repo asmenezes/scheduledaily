1d6ae30d3a06f354013ece5889d7306d6ecd95ff
asmenezes <asmenezes23@gmail.com>
2020-04-19 21:04:04 -0400

    Change Router To Switch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./App.css";
 import Main from "./components/creation/main.js";
 import ScheduleMain from "./components/schedule/schedulemain.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" render={(props) => <Main />} />
-      <Route exact path="/schedule" render={(props) => <ScheduleMain />} />
+      <Switch>
+        <Route path="/schedule">
+          <ScheduleMain />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
+// <Router>
+//   <Route exact path="/" render={(props) => <Main />} />
+//   <Route exact path="/schedule" render={(props) => <ScheduleMain />} />
+// </Router>
