30bf5676337040df61186c6788d55d48b1d65961
asmenezes <asmenezes23@gmail.com>
2020-04-19 21:51:30 -0400

    Updates

---
+++ precache-manifest.477f99e7fc1c7f80ed912a80c51332ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de568e187607fc183b5f1687079cc991",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4288237d0d444d8d577d",
+    "url": "./static/css/main.7962373a.chunk.css"
+  },
+  {
+    "revision": "562c2c62fa3fdaafef24",
+    "url": "./static/js/2.3c18a3c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3c18a3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4288237d0d444d8d577d",
+    "url": "./static/js/main.cd96a0ea.chunk.js"
+  },
+  {
+    "revision": "74c5ccb13945e8f60bc0",
+    "url": "./static/js/runtime-main.ac167c34.js"
+  }
+]);
