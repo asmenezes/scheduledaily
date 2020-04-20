85a7a4f7e95f23cdd964edae40c6fbb9848999cc
asmenezes <asmenezes23@gmail.com>
2020-04-19 22:22:50 -0400

    Fixed a route

--- src/components/schedule/scheduleheader.js
+++ src/components/schedule/scheduleheader.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function ScheduleHeader() {
   let today = new Date();
   today = today.toDateString();
-  return(
+  return (
     <React.Fragment>
-        <div class="scheduleheader">
-        <div className = "timegroup">
+      <div class="scheduleheader">
+        <div className="timegroup">
           <span>12:00</span>
           <span id="currenttimedisplay">12:30</span>
           <span>12:45</span>
         </div>
-        <div className = "baseline">
+        <div className="baseline">
           <span>{today}</span>
-          <a className="right" href="/">Schedule Creator</a>
-          </div>
-          <div id = "progressbar">
-            <div id = "progress">
-            </div>
-          </div>
+          <Link className="right" to="/">
+            Schedule Creator
+          </Link>
         </div>
-      </React.Fragment>
-    );
-
+        <div id="progressbar">
+          <div id="progress"></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default ScheduleHeader;
