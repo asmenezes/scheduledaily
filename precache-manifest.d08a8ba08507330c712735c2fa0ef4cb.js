e0cf42301bb5c7bb460011ca89f5fd64aefa3e2f
asmenezes <asmenezes23@gmail.com>
2020-04-20 20:34:46 -0400

    Updates

--- precache-manifest.d08a8ba08507330c712735c2fa0ef4cb.js
+++ precache-manifest.d08a8ba08507330c712735c2fa0ef4cb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4a77c4bc5960ea507feb8323e40feed",
+    "revision": "dcdf344eeb7762648f993857b4d2f425",
     "url": "/scheduledaily/index.html"
   },
   {
-    "revision": "801a54f746c12cb570e1",
+    "revision": "83d7f24b456f2ce1cf34",
     "url": "/scheduledaily/static/css/main.e6bd0810.chunk.css"
   },
   {
-    "revision": "5786736a9d5fc63bd9e7",
-    "url": "/scheduledaily/static/js/2.fa379d8c.chunk.js"
+    "revision": "edbff59592d24473b9ca",
+    "url": "/scheduledaily/static/js/2.2f57a07f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/scheduledaily/static/js/2.fa379d8c.chunk.js.LICENSE.txt"
+    "url": "/scheduledaily/static/js/2.2f57a07f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "801a54f746c12cb570e1",
-    "url": "/scheduledaily/static/js/main.8902178c.chunk.js"
+    "revision": "83d7f24b456f2ce1cf34",
+    "url": "/scheduledaily/static/js/main.e05e72fa.chunk.js"
   },
   {
     "revision": "02d5cead4f82971acc84",
