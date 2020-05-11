65326d5c83a0ceb3f5c6e114bda474f4acff4ec1
asmenezes <asmenezes23@gmail.com>
2020-05-11 17:32:50 -0400

    new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scheduledaily/precache-manifest.98b8982e39787ac72737a00e5a7ae9b4.js"
+  "/scheduledaily/precache-manifest.0ed50e9ad5796a52e3d17e8e663497e1.js"
 );
 
 self.addEventListener('message', (event) => {
