c9edc263cb394e49c801d060999a898d487a2819
asmenezes <asmenezes23@gmail.com>
2020-05-16 10:57:29 -0400

    Cleaned up code

--- src/components/scripts/shared.js
+++ src/components/scripts/shared.js
@@ -5,7 +5,6 @@ export const dateConv = (time) => {
   return today;
 };
 export const disp12Time = (timeString) => {
-  //if 1st 2 are more than 12 subtract 11
   let hours = Number(timeString.substring(0,2));
   if(hours > 12){
     hours -= 12;
