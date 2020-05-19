e7fe1d4f8fc6a8dcfc3805cfa4d6e853225dcaa2
asmenezes <asmenezes23@gmail.com>
2020-05-19 11:58:26 -0400

    Fix Time Gap and Cleaned up code

--- src/contexts/daynotescontext.js
+++ src/contexts/daynotescontext.js
@@ -1,19 +1,18 @@
 import React, { createContext, useEffect, useState } from "react";
 
-
 export const DayNotesContext = createContext();
 
 const DayNotesContextProvider = (props) => {
-
-let initNote = localStorage.getItem("note")
-let jsonNote = JSON.parse(initNote)[0]? JSON.parse(initNote) : [{note:"Today's Notes: "}]
-    const [note, setNote] = useState(jsonNote)
-    const editNote = (newNote) => {
-      setNote([{note:newNote}]);
-    }
+  let initNote = localStorage.getItem("note");
+  let jsonNote = JSON.parse(initNote)[0]
+    ? JSON.parse(initNote)
+    : [{ note: "Today's Notes: " }];
+  const [note, setNote] = useState(jsonNote);
+  const editNote = (newNote) => {
+    setNote([{ note: newNote }]);
+  };
 
   useEffect(() => {
-
     localStorage.setItem("note", JSON.stringify(note));
     console.log(`Notes was just set to ${JSON.stringify(note)}`);
   }, [note]);
