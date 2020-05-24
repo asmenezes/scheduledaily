18365ce7c8d018999b9db82b3808ed261a2dc441
asmenezes <asmenezes23@gmail.com>
2020-05-24 12:36:13 -0400

    Added Upcoming Tasks Logic

--- src/components/schedule/upcominglist.js
+++ src/components/schedule/upcominglist.js
@@ -1,19 +1,20 @@
 import React, { useContext } from "react";
 import TaskBlock from "../taskblock";
 import { ScheduleContext } from "../../contexts/schedulecontext";
+import { dateConv } from "../scripts/shared";
 
 function UpcomingList() {
+  let now = new Date();
   const { tasks } = useContext(ScheduleContext);
   return (
     <div id="upcominglist" className="scroll">
       <h3 className="lightlabel">Coming Up:</h3>
       {tasks[0] ? (
         <ul className="scroll">
-          {
-            // if task.startTime is more than now's time show it
-          }
           {tasks.map((task) => {
-            return <TaskBlock task={task} key={task.tID} />;
+            return dateConv(task.endTime.toString()) > now ? (
+              <TaskBlock task={task} key={task.tID} />
+            ) : null;
           })}
         </ul>
       ) : null}
