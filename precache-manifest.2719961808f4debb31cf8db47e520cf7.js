6c16dfa4e289963b8f5da010d8918b0085f8c47d
asmenezes <asmenezes23@gmail.com>
2020-05-31 09:50:34 -0400

    Updates

---
+++ precache-manifest.2719961808f4debb31cf8db47e520cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a229bd77d3d5ef1b63fd2f0b3d7b52d6",
+    "url": "/scheduledaily/index.html"
+  },
+  {
+    "revision": "4d0569ae8fce494f8378",
+    "url": "/scheduledaily/static/css/main.fdc7932b.chunk.css"
+  },
+  {
+    "revision": "4eee52d5bfddf37ccd0a",
+    "url": "/scheduledaily/static/js/2.2ef7976d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/scheduledaily/static/js/2.2ef7976d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0569ae8fce494f8378",
+    "url": "/scheduledaily/static/js/main.c9cd2bd1.chunk.js"
+  },
+  {
+    "revision": "02d5cead4f82971acc84",
+    "url": "/scheduledaily/static/js/runtime-main.5df808ee.js"
+  }
+]);
