cf39cc5da6e637e0a2f9b0eb4333dc573686a2c5
asmenezes <asmenezes23@gmail.com>
2020-07-05 11:23:52 -0400

    Updates

--- precache-manifest.ebfba6b98090563488815e59670b931b.js
+++ precache-manifest.ebfba6b98090563488815e59670b931b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32bb81126b37fb51ade448d0645c36c1",
+    "revision": "cafef752fe77ff88dc1f61ba3657f072",
     "url": "/scheduledaily/index.html"
   },
   {
-    "revision": "3672badf587d3b0701ba",
-    "url": "/scheduledaily/static/css/main.42b0034c.chunk.css"
+    "revision": "27df93664c4232abe9d6",
+    "url": "/scheduledaily/static/css/main.83915097.chunk.css"
   },
   {
     "revision": "4eee52d5bfddf37ccd0a",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/scheduledaily/static/js/2.2ef7976d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3672badf587d3b0701ba",
-    "url": "/scheduledaily/static/js/main.0b08449a.chunk.js"
+    "revision": "27df93664c4232abe9d6",
+    "url": "/scheduledaily/static/js/main.35112070.chunk.js"
   },
   {
     "revision": "02d5cead4f82971acc84",
