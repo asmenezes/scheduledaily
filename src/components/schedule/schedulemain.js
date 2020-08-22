9f20dd876cd47157cfbfbb94ac27750a0059b489
asmenezes <asmenezes23@gmail.com>
2020-08-22 11:02:07 -0400

    More responsiveness

--- src/components/schedule/schedulemain.js
+++ src/components/schedule/schedulemain.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ScheduleHeader from "./scheduleheader";
 import CurrentDisplay from "./currenttaskdisplay";
 import BooleanList from "./booleanlist";
@@ -9,6 +9,40 @@ import DayNotesContextProvider from "../../contexts/daynotescontext";
 import BooleanContextProvider from "../../contexts/booleancontext";
 
 function ScheduleMain() {
+  function hider(x) {
+    let thisOne = document.getElementById("booleanlist");
+    let thistwo = document.getElementById("upcominglist");
+    let thisthree = document.getElementsByClassName("timegroup");
+    let thisfour = document.getElementById("progressbar");
+    let thisone = [thisOne, thistwo, ...thisthree, thisfour];
+    if (x.matches) {
+      for (let i = 0; i < thisone.length; i++) {
+        console.log(thisone[i]);
+        if (!thisone[i].classList.contains("hide")) {
+          thisone[i].classList.add("hide");
+          //Finish subtask in reducer
+        } else {
+          //thisone[i].classList.remove("hide");
+          //unfinish subtask in reducer
+        }
+      }
+    } else {
+      for (let i = 0; i < thisone.length; i++) {
+        console.log(thisone[i]);
+        if (thisone[i].classList.contains("hide")) {
+          thisone[i].classList.remove("hide");
+          //Finish subtask in reducer
+        }
+      }
+    }
+  }
+
+  let x = window.matchMedia("(max-width: 720px)");
+  // Call listener function at run time
+  x.addListener(hider);
+  useEffect(() => {
+    hider(x);
+  }, []);
   return (
     <div>
       <ScheduleContextProvider>
